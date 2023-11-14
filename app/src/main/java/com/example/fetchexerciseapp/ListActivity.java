72711527d988a983f629c91044460d11b6b46b2a
wkopec5 <77519227+wkopec5@users.noreply.github.com>
2023-11-14 16:14:00 -0600

    Commited repo to GitHub
    Commited repo to GitHub

---
+++ app/src/main/java/com/example/fetchexerciseapp/ListActivity.java
@@ -0,0 +1,154 @@
+package com.example.fetchexerciseapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ListActivity extends AppCompatActivity {
+
+    private LinkedHashMap<String, GroupInfo> productsArray = new LinkedHashMap<String, GroupInfo>();
+    private ArrayList<GroupInfo> listIdsArray = new ArrayList<GroupInfo>();
+
+    private CustomAdapter listAdapter;
+    private ExpandableListView simpleExpandableListView;
+    public static Map<Object, List<record>> sortedMap;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        //Get Display List button and set onClick to show list page
+        Button homeButton = (Button) findViewById(R.id.homeButton);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //Get Display List button and set onClick to show list page
+        Button printButton = (Button) findViewById(R.id.printButton);
+        printButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sortedMap.forEach((key, value) ->
+                        //Access the list in each key and add the lists of each listId to the Extendable ListView
+                        value.forEach((v) ->
+                                Log.d("my-api", "ListID: " + key.toString() + " Product Name : " + v.getName() + " Product ID: " + Integer.toString(v.getId()))
+                        )
+                );
+            }
+        });
+
+        //Iterate through the sorted listIds (Key) and List<record> value in each key.
+        //sortedMap was created by getData() function in MainActivity.java.
+        //In getData function, we sorted the data appropriately and stored it
+        //into Map<Object, List<record>> sortedMap.
+        sortedMap.forEach((key, value) ->
+                //Access the list in each key and add the lists of each listId to the Extendable ListView
+                value.forEach((v) ->
+                        addProduct(key.toString(), v.getName(), Integer.toString(v.getId()))
+                )
+        );
+
+        //Get the extendable list view
+        simpleExpandableListView = (ExpandableListView) findViewById(R.id.simpleExpandableListView);
+        //Create the custom adapter for the extendable list
+        listAdapter = new CustomAdapter(ListActivity.this, listIdsArray);
+        //Set the adapter for the extendable list view
+        simpleExpandableListView.setAdapter(listAdapter);
+
+        //Uncomment if you want to expand all groups immediately
+        //expandAll();
+
+        //SetOnChildClickListener listener for child row click
+        simpleExpandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+                //get the group header
+                GroupInfo headerInfo = listIdsArray.get(groupPosition);
+                //get the child info
+                ChildInfo productInfo = headerInfo.getProductList().get(childPosition);
+                //display it or do something with it
+                Toast.makeText(getBaseContext(), " Tapped Item " + productInfo.getName() + " , In ListID: " + headerInfo.getName(), Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
+
+        //A listener for ListID heading click
+        simpleExpandableListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+                //get the ListID Header
+                GroupInfo headerInfo = listIdsArray.get(groupPosition);
+                //display it or do something with it
+                Toast.makeText(getBaseContext(), " Tapped ListIds: " + headerInfo.getName(),
+                        Toast.LENGTH_LONG).show();
+
+                return false;
+            }
+        });
+
+
+    }
+
+    //A method to expand all groups
+    private void expandAll() {
+        int count = listAdapter.getGroupCount();
+        for (int i = 0; i < count; i++) {
+            simpleExpandableListView.expandGroup(i);
+        }
+    }
+
+    //A method to collapse all groups
+    private void collapseAll() {
+        int count = listAdapter.getGroupCount();
+        for (int i = 0; i < count; i++) {
+            simpleExpandableListView.collapseGroup(i);
+        }
+    }
+
+
+    //A method to add each item into the extendable list view
+    private int addProduct(String listId, String id, String name) {
+
+        int groupPosition = 0;
+
+        //Check the map if the group already exists
+        GroupInfo headerInfo = productsArray.get(listId);
+        //Add the group if doesn't exist
+        if (headerInfo == null) {
+            headerInfo = new GroupInfo();
+            headerInfo.setName(listId);
+            productsArray.put(listId, headerInfo);
+            listIdsArray.add(headerInfo);
+        }
+
+        ArrayList<ChildInfo> productList = headerInfo.getProductList();
+
+        //Create a new product and add that it's listId group
+        ChildInfo productInfo = new ChildInfo();
+        productInfo.setlistId(id);
+        productInfo.setName(name);
+        productInfo.setId(listId);
+        productList.add(productInfo);
+        headerInfo.setProductList(productList);
+
+        //find the group position inside the list
+        groupPosition = listIdsArray.indexOf(headerInfo);
+        return groupPosition;
+    }
+}
---
+++ app/src/main/java/com/example/fetchexerciseapp/MainActivity.java
@@ -0,0 +1,138 @@
+package com.example.fetchexerciseapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class MainActivity extends AppCompatActivity {
+
+    final private ArrayList<record> allData = new ArrayList<>();
+
+    private Map<Object, List<record>> recordsGrouped;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Retrieve the data from the link, put it into the ArrayList, and set the sortedMap in ListActivity to the sorted data
+        getData("https://fetch-hiring.s3.amazonaws.com/hiring.json");
+
+        Log.d("my-api", "onCreate was called!");
+
+
+        //Get Display List button and set onClick to show list page
+        Button displayListBtn = (Button) findViewById(R.id.button);
+        displayListBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ListActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    public void getData(String apiURL){
+        /*
+        Query the data from the link
+        */
+        RequestQueue requestQueue = null;
+        if(requestQueue == null){
+            requestQueue = Volley.newRequestQueue(this);
+        }
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET,apiURL,null,new Response.Listener<JSONArray>(){
+            @Override
+            public void onResponse(JSONArray response) {
+                try {
+                    //go through the data and add the data to the arraylist (Should be custom per API attributes)
+                    for (int i=0;i<response.length();i++){
+                        JSONObject jsonObject = response.getJSONObject(i);
+                        allData.add(new record(Integer.parseInt(jsonObject.getString("id")),Integer.parseInt(jsonObject.getString("listId")),jsonObject.getString("name")));
+                    }
+
+
+
+                    Intent listIntent = new Intent(MainActivity.this, ListActivity.class);
+
+                    //Filter out and sort all of the data as needed
+                    recordsGrouped = allData.stream().filter((record) -> record.getName() != "null") //filter out all names with null
+                            .filter((record) -> record.getName().length() >= 1)                                                 //filter out all the names with no text
+                            .sorted(Comparator.comparing(record::getListId))                                               //first sorting ArrayList by listId
+                            .sorted(Comparator.comparing(record::getName))                                                 //second sorting ArrayList by record names
+                            .collect(Collectors.groupingBy(w -> w.getListId()));                                                //group the lists by listIds and set list Id groups to the value of the listId key in the map
+
+                    //Set the ListActivity's sorted map to the grouped map created above
+                    ListActivity.sortedMap = recordsGrouped;
+
+
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        },new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("my-api","Something Went Wrong! :(");
+            }
+        });
+        requestQueue.add(jsonArrayRequest);
+    }
+
+
+
+}
+/*
+
+--------- Class: Record ----------------
+A class created to store the API info. Includes a Constructor, getters, and setters.
+
+*/
+class record {
+    private final int id;
+    private final int listId;
+    private final String name;
+
+    public record(int id, int listId, String name){
+        this.id = id;
+        this.listId = listId;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getListId() {
+        return listId;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+}
